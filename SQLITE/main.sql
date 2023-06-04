a7142246141994ab8d8bf5be8503bb55bf32a6f6
LIRA-MIGUEL <122810468+LIRA-MIGUEL@users.noreply.github.com>
2023-06-04 21:41:34 +0000

    Se actiualizo la carpeta SQLITE con el importe de las tablas y la creación del índice

--- SQLITE/main.sql
+++ SQLITE/main.sql
@@ -1,30 +1,31 @@
-CREATE TABLE clientes (
+CREATE TABLE IF NOT EXISTS clientes (
     id_cliente integer PRIMARY KEY AUTOINCREMENT,
     nombre varchar(50),
     email varchar(50)
 );
-SELECT * FROM clientes;
 
-CREATE TABLE ventas(
-    id_venta integer PRIMARY KEY AUTOINCREMENT,
-    fecha date,
-    id_cliente integer REFERENCES clientes(id_cliente)
-);
-SELECT * FROM ventas;
-
-CREATE TABLE detalle_ventas(
-    id_detalle_venta integer PRIMARY KEY AUTOINCREMENT,
-    id_venta integer REFERENCES ventas(id_venta),
-    id_producto integer REFERENCES productos(id_producto),
-    cantidad_producto integer,
-    precio_unitario float,
-    total_x_producto float
-);
-SELECT * FROM detalle_ventas;
-
-CREATE TABLE productos(
+CREATE TABLE IF NOT EXISTS productos(
     id_producto integer PRIMARY KEY AUTOINCREMENT,
     producto varchar(50),
     precio_unitario float
 );
+
+CREATE TABLE IF NOT EXISTS ventas(
+    id_venta integer PRIMARY KEY AUTOINCREMENT,
+    fecha date,
+    FOREING KEY (id_cliente) REFERENCES clientes(id_cliente)
+);
+
+CREATE TABLE IF NOT EXISTS detalle_ventas(
+    id_detalle_venta integer PRIMARY KEY AUTOINCREMENT,
+    FOREING KEY (id_venta) REFERENCES ventas(id_venta),
+    FOREING KEY (id_producto) integer REFERENCES productos(id_producto),
+    cantidad_producto integer,
+    precio_unitario float,
+    total_x_producto float
+);
+
+SELECT * FROM clientes;
 SELECT * FROM productos;
+SELECT * FROM ventas;
+SELECT * FROM detalle_ventas;
